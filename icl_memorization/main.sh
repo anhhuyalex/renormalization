e55425f48127f60bf9c4054fbd64389591192aae
anhhuyalex <alexn@minerva.kgi.edu>
2025-11-10 03:33:18 -0500

    fix num_hidden_features = [1] + list(range(2, 17, 2)) # len: 9

--- icl_memorization/main.sh
+++ icl_memorization/main.sh
@@ -6,12 +6,12 @@
 #SBATCH -o slurms/%j-with-projection-layer-with-residual-mlp.out
 #SBATCH --gres=gpu:1
 #SBATCH --array=0-99
-# SBATCH --partition=mig
+#SBATCH --partition=mig
 # source activate renormalization
 source ~/.bashrc
-# source ../../learning_to_learn/l2l/bin/activate
+source ../../learning_to_learn/l2l/bin/activate
 # cd /jukebox/norman/qanguyen/patdiff_seq
-conda activate /mnt/cup/labs/norman/qanguyen/patdiff_seq/fmri
+# conda activate /mnt/cup/labs/norman/qanguyen/patdiff_seq/fmri
 
 # wandb login --relogin --host=https://stability.wandb.io
 # srun --pty -p della-gpu -c 2 -t 4:00:00 --gres=gpu:1 --mem-per-cpu=20G bash
